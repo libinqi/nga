a38aee5d660bce11937ca6c3388f7598b2990346
libinqi <libinqi@outlook.com>
2017-06-25 17:19:04 +0800

    更新angular版本至4.1.x

---
+++ templates/src/app/no-content/no-content.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'no-content',
+  template: `
+    <div>
+      <h1>404: 页面未找到</h1>
+    </div>
+  `
+})
+export class NoContentComponent {
+
+}
